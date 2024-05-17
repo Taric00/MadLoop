e82ab9a572ca1f0cf2530013d35988a97391fbbc
Taric00 <albayrakmehmettarik@gmail.com>
2024-05-17 20:19:10 +0300

    Menu

---
+++ Assets/Scripts/MenuScripts/PlayButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+
+    public void ChangeScene()
+    {
+        //SceneManager.LoadScene("oynu_sahnesi");
+        SceneManager.LoadScene(1);
+    }
+
+}
