83f5a62848d3148fab89332bbe0ecbc11a8d9a74
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2024-05-18 01:41:19 +0300

    tba

--- Assets/Scripts/ObjectScripts/DoorScript.cs
+++ Assets/Scripts/ObjectScripts/DoorScript.cs
@@ -6,9 +6,22 @@ namespace ObjectScripts
     public class DoorScript : MonoBehaviour,Iinteractable
     {
         public Animator Animator;
+        private bool isAlreadyOpened;
+        
         public void CanInteractable()
         {
-            Animator.SetBool("isOpen",true);
+            if (!isAlreadyOpened)
+            {
+                Animator.SetBool("CloseDoor",false);
+                Animator.SetBool("OpenDoor",true);
+                isAlreadyOpened = true;
+            }
+            else
+            {
+                Animator.SetBool("OpenDoor",false);
+                Animator.SetBool("CloseDoor",true);
+                isAlreadyOpened = false;
+            }
         }
     }
 }
