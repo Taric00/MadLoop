8aa9d677a30dddb6ed52bb31b56782165130d494
Muhammet Hamit KARAYILAN <muhammethamitkarayilan@gmail.com>
2024-05-18 12:33:30 +0300

    animation scene added

--- Assets/Scripts/ObjectScripts/Letter.cs
+++ Assets/Scripts/ObjectScripts/Letter.cs
@@ -1,16 +1,31 @@
 using Interfaces;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ObjectScripts
 {
     public class Letter : MonoBehaviour,Iinteractable
     {
-        public GameObject LetterText;
+        public GameObject LetterImage;
         public GameObject Point;
         public void CanInteractable()
         {
-            LetterText.SetActive(true);
+            LetterImage.SetActive(true);
             Point.SetActive(false);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
+
+        public void OnClickCloseLetter()
+        {
+            LetterImage.SetActive(false);
+            Point.SetActive(true);
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        
+        
     }
 }
